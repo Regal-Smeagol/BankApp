ff60733645e4cc3489bda9af7d71e4c7edec5487
Russell Brown <rbrown1995@protonmail.com>
2021-11-17 17:04:23 -0600

    outline for bank application

---
+++ BankApp/bank.py
@@ -0,0 +1,57 @@
+print("hello world")
+
+def create_account():
+    pass
+
+def get_account_by_id():
+    pass
+
+def deposit():
+    pass
+
+def withdraw():
+    pass
+
+def transfer():
+    pass
+
+def update_customer_info():
+    pass
+
+def retrieve_customer_info():
+    pass
+
+def close_account():
+    pass
+
+def customer_departure():
+    pass
+
+def transaction_validation():
+    pass
+
+def add_customer():
+    pass
+
+def retrieve_list_of_customers():
+    pass
+
+def retrieve_list_of_accounts():
+    pass
+
+def retrieve_list_of_accounts_by_cust_id():
+    pass
+
+
+class Customer():
+    pass
+
+class Account():
+    pass
+    # account num
+    # customer ID
+    # Balance
+    # On hold or not
+    # limit
+    # interest rate if any
+    #
